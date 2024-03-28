7d2cafacb53fb9e4498aeb1239744f91e8e1d08b
Normal_Pete <petrohorokh@gmail.com>
2024-03-28 16:33:27 +0200

    Implemented Automapper

---
+++ Rent.BLL/Profiles/TenantProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Rent.DAL.DTO;
+using Rent.DAL.Models;
+
+namespace Rent.BLL.Profiles;
+
+public class TenantProfile : MappingProfile
+{
+    public TenantProfile()
+    {
+        CreateMap<Tenant, TenantToGetDto>();
+
+        CreateMap<TenantToCreateDto, TenantToUpdateDto>();
+    }
+}
